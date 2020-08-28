c085cf7c5f35cc9474960cd0659b22c8f6f964e8
Ezz <mhmd.ezz@gmail.com>
2020-08-29 00:19:50 +1000

    Setting up the project

---
+++ src/main/java/au/com/momenton/EmployeeHierarchyApplication.java
@@ -0,0 +1,12 @@
+package au.com.momenton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeHierarchyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeHierarchyApplication.class, args);
+	}
+}
